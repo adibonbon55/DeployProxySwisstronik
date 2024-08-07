c1e00bc43d21a691f6263d4847f512782fed3ea0
adibonbon55 <adiadikaze12@gmail.com>
2024-08-07 14:18:58 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x2a0D7E8cf62a4879234CF95500520B8A98ef983D'
+export const ProxyAdmin = '0x852eb1a62a5fFB793642EfC44F5E3ff34A1436Af'
+export const SWTRImplementation = '0x5DEcda3Eaa3263E826f8Ea498dF679D606b0E00f'
     
